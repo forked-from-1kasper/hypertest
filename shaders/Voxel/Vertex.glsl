d8c432dfd15f5265043a34e96cb1d245d8ec20ef
Siegmentation Fault <siegmentationfault@yandex.ru>
2024-11-28 21:39:30 +0700

    shaders/Voxel/Vertex.glsl: clean up

--- shaders/Voxel/Vertex.glsl
+++ shaders/Voxel/Vertex.glsl
@@ -1,6 +1,5 @@
 in  vec2  _texCoord;
 in  vec3  _vertex;
-
 out vec2  texCoord;
 out float fogFactor;
 
@@ -12,18 +11,19 @@ uniform mat4 view;
 
 uniform Fog fog;
 
-float getFogFactor(in float d) {
+float getFogFactor(float d) {
     if (fog.enabled)
         return clamp(1.0 - (fog.far - d) / (fog.far - fog.near), 0.0, 1.0);
     else
         return 0.0;
 }
 
-void main() {
-    vec2 v = applyModel(apply(origin, apply(relative, _vertex.xy)));
-    vec4 vec = view * vec4(v.x, _vertex.z, v.y, 1.0);
+vec3 model(vec3 v) { return vec3(applyModel(apply(origin, apply(relative, v.xy))), v.z); }
 
-    gl_Position = projection * vec;
-    fogFactor   = getFogFactor(length(vec));
+void main() {
+    vec4 vertex = view * vec4(model(_vertex).xzy, 1.0);
+
+    gl_Position = projection * vertex;
+    fogFactor   = getFogFactor(length(vertex));
     texCoord    = _texCoord;
 }
